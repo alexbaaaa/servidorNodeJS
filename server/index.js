d634e79debca93911db06f4a495dab094be33f49
alexbaaaa <aalejandrodesarrollo@gmail.com>
2024-10-17 12:40:23 +0200

    paginas staticas

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = 3000;
+
 app.use(cors());
+app.use(express.static('archivos'));
 app.get('/', (req,res) => {
     let respuesta = {
         "mensaje": "Hola pepe"
